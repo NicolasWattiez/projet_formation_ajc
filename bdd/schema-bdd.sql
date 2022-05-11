45433ebd3a4e24438fd323db965b3ff15cecd929
Nicolas Wattiez <nicolaswattiez.contact@gmail.com>
2022-05-11 12:41:34 +0200

    reorganize project structure and add schema bdd

---
+++ bdd/schema-bdd.sql
@@ -0,0 +1,42 @@
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `pseudo` varchar(255),
+  `created_at` timestamp
+);
+
+CREATE TABLE `join_user_qcm` (
+  `user_id` int,
+  `qcm_id` int
+);
+
+CREATE TABLE `qcm` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255)
+);
+
+CREATE TABLE `join_qcm_questions` (
+  `question_id` int,
+  `qcm_id` int
+);
+
+CREATE TABLE `questions` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255)
+);
+
+CREATE TABLE `answers` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `answers` varchar(255),
+  `correct_answer` varchar(255),
+  `question_id` int
+);
+
+ALTER TABLE `join_user_qcm` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `join_user_qcm` ADD FOREIGN KEY (`qcm_id`) REFERENCES `qcm` (`id`);
+
+ALTER TABLE `join_qcm_questions` ADD FOREIGN KEY (`qcm_id`) REFERENCES `qcm` (`id`);
+
+ALTER TABLE `join_qcm_questions` ADD FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`);
+
+ALTER TABLE `answers` ADD FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`);
