43ae1c437fb90772daf70b06b586180c4f3958c6
AntoineOll <ollitrault.a@gmail.com>
2022-05-31 09:59:00 +0000

    question done

--- src/model/question.py
+++ src/model/question.py
@@ -27,29 +27,43 @@ class Question():
         return print_data
 
 
-    def insert_data(self):
+    def insert_data(self,question_values):
+        question=question_values["name"]
+        all_answers=question_values["answers"]
+        good_answer=question_values["correct_answer"]
         try:
             cursor=connexion.cursor()
-            cursor.execute('INSERT INTO questions (name,answers,correct_answer) VALUES ("test ?","a,b,c","c");')
+            cursor.execute('INSERT INTO questions (name,answers,correct_answer) VALUES (?,?,?);',
+            (question,all_answers,good_answer)
+            )
             connexion.commit()
             return 'La question a bien été ajoutée'
         except mariadb.Error as e:
             return 'Erreur lors de l\'ajout de la question {e} '
 
 
-    def remove_data(self):
+    def remove_data(self,id):
+        numero_id=id
         try:
             cursor = connexion.cursor()
-            cursor.execute('DELETE FROM questions WHERE id = "5";')
+            cursor.execute('DELETE FROM questions WHERE id = ?;',
+            (numero_id,)
+            )
             connexion.commit()
             return 'La question à bien été supprimée'
         except mariadb.Error as e:
             return 'Erreur lors de la suppression de la question {e}' 
 
-    def update_data(self):
+    def update_data(self,id,new_question_values):
+        numero_id=id 
+        new_question=new_question_values["name"] 
+        new_all_answers=new_question_values["answers"] 
+        new_good_answer=new_question_values["correct_answer"] 
         try:
             cursor = connexion.cursor()
-            cursor.execute('UPDATE questions SET `correct_answer` = "a" WHERE `id` = 6;')
+            cursor.execute('UPDATE questions SET `name` = ? , answers = ?, correct_answer = ? WHERE `id` = ?;',
+            (new_question,new_all_answers,new_good_answer,numero_id)
+            )
             connexion.commit()
             return 'La réponse a bien été modifiée'
         except mariadb.Error as e:
@@ -71,6 +85,8 @@ test_question = Question(cursor)
 
 # test_qcm.update_data()
 a=input('id de la question:')
-print(test_question.get_data(a))
+question_values = {"name": "Ta couleur favorite ?", "answers": "rouge,vert,noir", "correct_answer": "vert"}
+new_question_values ={"name": "Ta couleur favorite ?", "answers": "rouge,vert,noir", "correct_answer": "noir"}
+print(test_question.update_data(a,new_question_values))
 
 
